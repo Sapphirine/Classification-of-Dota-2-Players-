be803b0263dbd55c94c7117f6158539f53152dbe
Kevin Xing <uestcxzk@gmail.com>
2017-12-06 18:46:11 -0500

    done

--- Web/Server.py
+++ Web/Server.py
@@ -1,4 +1,7 @@
 from flask import Flask, request, render_template, jsonify
+import os
+import json
+
 app = Flask(__name__)
 
 
@@ -11,6 +14,15 @@ def index():
 def query():
 	steam_id = request.args.get('x', 0, type=int)
 	return jsonify(result=1)
+
+@app.route('/json')
+def graph_json():
+	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+	json_url = os.path.join(SITE_ROOT, "json", "graph.json")
+	json_data = json.load(open(json_url))
+	print json_data
+	return jsonify(json_data)
+
 
 
 
